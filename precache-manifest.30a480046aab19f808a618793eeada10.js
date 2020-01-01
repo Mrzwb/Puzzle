95368799273433c419f82985182528408684fa91
zhouwb15 <zhouwb15@chinauniocm.cn>
2020-01-01 21:50:50 +0800

    Updates

---
+++ precache-manifest.30a480046aab19f808a618793eeada10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e997839ce43397499519aa10137f7f4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "088702b9b6f69c37cd5a",
+    "url": "./static/css/main.c6c5b03d.chunk.css"
+  },
+  {
+    "revision": "9324314adbafab4aa560",
+    "url": "./static/js/2.864d59b4.chunk.js"
+  },
+  {
+    "revision": "088702b9b6f69c37cd5a",
+    "url": "./static/js/main.fed7e343.chunk.js"
+  },
+  {
+    "revision": "9035cabd660b0cb20e60",
+    "url": "./static/js/runtime-main.13d9ce90.js"
+  },
+  {
+    "revision": "ef2b6dff7e1a82fb1e9d9f1032b8ca5d",
+    "url": "./static/media/default.ef2b6dff.jpg"
+  }
+]);
